a12205a9df946ca0cc1e4383402428bfc9e88c12
Nick Sutterer <apotonick@gmail.com>
2016-02-08 18:29:47 +1300

    use callback to calculate count.

--- app/concepts/cart/operation/add.rb
+++ app/concepts/cart/operation/add.rb
@@ -3,6 +3,8 @@ class Cart < ActiveRecord::Base
   # Cart::Add.
   class Add < Trailblazer::Operation
     contract do
+      feature Disposable::Twin::Persisted
+
       collection :products, from: :items,
         populator: ->(fragment:, **) {
           products.append Item.new
@@ -13,6 +15,8 @@ class Cart < ActiveRecord::Base
           validates :id, presence: true
           validates :qty, presence: true, numericality: true
       end
+
+      property :count, default: 0
     end
 
     include Model
@@ -20,8 +24,25 @@ class Cart < ActiveRecord::Base
 
     def process(params)
       validate(params) do |contract|
+        # before_save!
+        dispatch!(:before_save)
         contract.save
       end
+    end
+
+  private
+    def before_save!
+      contract.count = contract.products.inject(0) { |memo, p| memo += p.qty }
+    end
+
+    callback :before_save do
+      collection :products do
+        on_add :increment_count!
+      end
+    end
+
+    def increment_count!(product, options)
+      contract.count += product.qty
     end
   end
 end
